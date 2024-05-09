ccc43caa19b82624ca4f7b593a6551849fc4c110
A1509 <erazoalexis16@gmail.com>
2024-05-09 03:46:34 -0500

    proyecto

---
+++ src/main/java/com/alexis/erazo/ErazoApplication.java
@@ -0,0 +1,13 @@
+package com.alexis.erazo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErazoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErazoApplication.class, args);
+	}
+
+}
